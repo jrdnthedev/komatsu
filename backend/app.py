7869e7042b6839feae702d958432bc40ef5f1097
andre mcmorris <ajmcmorris@gmail.com>
2025-05-10 13:23:34 -0400

    added connected genai responses to backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from google import genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+key = os.environ.get('GENAI_KEY')
+if not key:
+    raise ValueError("GENAI_KEY environment variable is not set")
+client = genai.Client(api_key=key)
 CORS(app)
 
 @app.route('/api/chat', methods=['POST'])
@@ -12,10 +20,12 @@ def chat():
     if not prompt:
         return jsonify({'error': 'Prompt is required'}), 400
 
+    response = client.models.generate_content(
+        model= os.environ.get('GENAI_MODEL'),
+        contents=[prompt],
+    )
     
-    mock_response = f"🤖 LLM Response: You said '{prompt}'"
-
-    return jsonify({'response': mock_response})
+    return jsonify({'response': response.text})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
